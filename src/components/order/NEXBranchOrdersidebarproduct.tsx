dae5681f53a1059d11f3015145ded3d02b723998
Luishiño <pericena12@gmail.com>
2024-04-20 10:58:35 -0400

    FOOD-33 Crear componentes reciclables (#9)
    * FOOD-33: Actualizar componentes banner , ver producto, lista de producto
    
    * FOOD-33: Se realizo reformatear el codigo, se elimino codigo comentado como archivos duplicados

---
+++ src/components/order/NEXBranchOrdersidebarproduct.tsx
@@ -0,0 +1,44 @@
+const product = {
+  name: 'Pollo',
+  img: '../../../public/images/food/11.jpg',
+  img1: '../../../public/images/food/9.jpg',
+  img2: '../../../public/images/food/10.jpg',
+  img3: '../../../public/images/food/2.jpg',
+
+  title: '30% Off',
+  subtitle: 'Comida de fitness',
+  subtitle2: 'a partir de',
+  subtitle3: '20.00',
+  subtitle4: 'Bs'
+}
+
+const NEXproduct = () => {
+  return (
+    <div className="blog-card">
+      <img src={product.img} alt={product.name} className="blog-banner" />
+      <p className="showcase-badge angle">stop</p>
+      <div className="blog-content">
+        <h3 className="blog-title">Pollo</h3>
+        <div className="price-box">
+          <p className="price">Precio: 27 Bs</p>
+          <del>30.00 Bs</del>
+        </div>
+        <a href="#" className="showcase-title">
+          {' '}
+          Pollo al horno
+        </a>
+        <div className="banner-btn-product">
+          <button className="add-to-cart-btn-add" data-name="Milaneza" data-price="20">
+            <i className="bx bxs-cart-add"></i>
+            Añadir
+          </button>
+          <button id="openModalBtn-ver" className="add-to-cart-btn-show">
+            <i className="bx bxs-show"></i> Ver
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NEXproduct
