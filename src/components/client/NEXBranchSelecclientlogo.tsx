04b50d9853f9751b13764dc67c4318ce0aa058a8
Luishiño Pericena Choque <pericena12@gmail.com>
2024-04-20 14:11:48 -0400

    FOOD-32: Se creo nuevos componentes logo, buscador, usuario - cerrar session, configuracion. Se elimino archivos duplicados , nuevo estilo css

---
+++ src/components/client/NEXBranchSelecclientlogo.tsx
@@ -0,0 +1,26 @@
+
+const logo = {
+	name: 'KFC',
+	img: '../../../public/images/cliente/Logo-KFC.png',
+	branch: 'Sucursal 4to anillo, av 4to anillo, 3 pasos al frente',
+	width: 600,
+	height: 500
+};
+
+
+const NEXlogo = () => {
+	return (
+		<div>
+			<h3>
+				<button className="back-btn">
+					<i className="bx bx-arrow-back"></i> Volver</button>
+				<p>{logo.name}</p>
+			</h3>
+			<p>"Puntos de ventas"</p>
+			<img src={logo.img} alt={logo.name} className="image" width={logo.width} height={logo.height} />
+		</div>
+
+	);
+}
+
+export default NEXlogo;
