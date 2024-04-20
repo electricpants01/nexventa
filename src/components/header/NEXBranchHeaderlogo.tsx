08f6e1ff453fef5d89e8b864d03853b9d7d7c676
Luishiño Pericena Choque <pericena12@gmail.com>
2024-04-20 11:14:55 -0400

    FOOD-32: Se creo nuevos componentes, logo, buscador, usuario

---
+++ src/components/header/NEXBranchHeaderlogo.tsx
@@ -0,0 +1,13 @@
+const logo = {
+  name: 'KFC',
+  img: '../../../public/images/cliente/Logo-KFC.png'
+}
+const NEXlogo = () => {
+  return (
+    <a href="#" className="header-logo">
+      <img src={logo.img} alt={logo.name} width="70" height="50" />
+    </a>
+  )
+}
+
+export default NEXlogo
