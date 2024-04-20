45e8aea8f942736538c8abb1256f510ab4ca6212
Luishiño Pericena Choque <pericena12@gmail.com>
2024-04-20 14:41:36 -0400

    FOOD-32: Se idento el codigo comentados

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-        const toggle = document.getElementById('nav-toggle')
-        const navbar = document.getElementById('navbar')
+  const toggle = document.getElementById('nav-toggle')
+  const navbar = document.getElementById('navbar')
 
-        toggle.addEventListener('click', function () {
-            navbar.classList.toggle('expander')
-        })
+  toggle.addEventListener('click', function () {
+    navbar.classList.toggle('expander')
+  })
 
-        // Ocultar el menú cuando haces clic fuera de él
-        window.addEventListener('click', function (event) {
-            if (!navbar.contains(event.target) && !toggle.contains(event.target)) {
-                navbar.classList.remove('expander')
-            }
-        })
-    })
+  // Ocultar el menú cuando haces clic fuera de él
+  window.addEventListener('click', function (event) {
+    if (!navbar.contains(event.target) && !toggle.contains(event.target)) {
+      navbar.classList.remove('expander')
+    }
+  })
+})
