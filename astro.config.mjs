041413039ec09ef33caab34bc4931a104f54e300
Christian Torrico Avila <christorricoavila@gmail.com>
2024-05-06 05:14:34 -0700

    added amplify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import awsAmplify from 'astro-aws-amplify';
 
 import node from "@astrojs/node";
 
@@ -8,7 +9,5 @@ import node from "@astrojs/node";
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: awsAmplify(),
 });
