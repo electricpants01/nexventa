7ee1105c6f224fb45df7c593c19117ca865bf5f8
Christian Torrico <christian.torrico-contractor@procore.com>
2024-05-15 00:05:46 -0400

    updated amplify.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,6 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "hybrid",
-  adapter: node({
-    mode: "standalone"
-  })
+  output: "server",
+  adapter: awsAmplify(),
 });
