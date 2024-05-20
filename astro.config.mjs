96fe1c0a4f898fff86e588aa331ab8036859916b
Christian Torrico Avila <christorricoavila@gmail.com>
2024-05-19 20:12:08 -0400

    configured app to be hosted in the cloud

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import awsAmplify from 'astro-aws-amplify';
+import aws from 'astro-sst';
 
 import node from "@astrojs/node";
 
@@ -9,5 +10,7 @@ import node from "@astrojs/node";
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: awsAmplify(),
+  adapter: aws({
+    deploymentStrategy: "edge",
+  }),
 });
