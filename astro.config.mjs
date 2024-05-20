9aaacd4d960b338539425afa4889faf6a4da73da
Christian Torrico Avila <christorricoavila@gmail.com>
2024-05-19 20:18:06 -0400

    removed amplify on astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import awsAmplify from 'astro-aws-amplify';
 import aws from 'astro-sst';
-
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
