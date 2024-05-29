467357a47a9391d82ad37b4745c1ba5007d66878
Luishiño <pericena12@gmail.com>
2024-05-29 11:26:17 -0400

    FOOD-31: Se mejoro el archivo json de earning para que sea mas dinamico , se podra cambiar los colores , titulo , entre otros .

--- src/components/admin/overview/card/earning/EarningData.tsx
+++ src/components/admin/overview/card/earning/EarningData.tsx
@@ -1,15 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Line } from 'react-chartjs-2';
-import {
-  Chart as ChartJS,
-  LineElement,
-  PointElement,
-  LinearScale,
-  Title,
-  CategoryScale,
-  Tooltip,
-  type ChartData,
-} from 'chart.js';
+import { Chart as ChartJS, LineElement, PointElement, LinearScale, Title, CategoryScale, Tooltip, type ChartData } from 'chart.js';
 
 ChartJS.register(
   LineElement,
@@ -21,9 +12,17 @@ ChartJS.register(
 );
 
 interface EarningData {
-  xValues: number[];
-  y1Values: number[];
-  y2Values: number[];
+  labels: number[];
+  datasets: {
+    label: string;
+    borderColor: string;
+    backgroundColor: string;
+    borderWidth: number;
+    data: number[];
+    pointRadius: number;
+    tension: number;
+    fill: boolean;
+  }[];
 }
 
 const EarningData: React.FC = () => {
@@ -32,58 +31,29 @@ const EarningData: React.FC = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    async function fetchDataAndDrawChart() {
+    const fetchDataAndDrawChart = async () => {
       try {
         const response = await fetch('/api/earning.json');
-        if (response.ok) {
-          const data: EarningData = await response.json();
-          if (data) {
-            setData({
-              labels: data.xValues,
-              datasets: [
-                {
-                  label: 'Dataset 1',
-                  borderColor: 'rgba(1, 212, 146, 1)',
-                  backgroundColor: 'rgba(255, 255, 255, 0.6)', // Opacidad blanco
-                  borderWidth: 2,
-                  data: data.y1Values,
-                  pointRadius: 0,
-                  tension: 0.4,
-                  fill: false
-                },
-                {
-                  label: 'Dataset 2',
-                  borderColor: 'rgba(197, 69, 89, 1)',
-                  backgroundColor: 'rgba(255, 255, 255, 0.6)', // Opacidad blanco
-                  borderWidth: 2,
-                  data: data.y2Values,
-                  pointRadius: 0,
-                  tension: 0.4,
-                  fill: false
-                }
-              ]
-            });
-          } else {
-            throw new Error('La respuesta está vacía');
-          }
-        } else if (response.status === 400) {
-          throw new Error('Error: Solicitud incorrecta (400)');
-        } else if (response.status === 404) {
-          throw new Error('Error: Recurso no encontrado (404)');
-        } else if (response.status === 500) {
-          throw new Error('Error: Error interno del servidor (500)');
-        } else {
-          throw new Error(
-            'Error al cargar los datos. Estado: ' + response.status + ' ' + response.statusText
-          );
+        if (!response.ok) {
+          throw new Error(`Error al cargar los datos. Estado: ${response.status} ${response.statusText}`);
         }
+        const earningData: EarningData = await response.json();
+        setData({
+          labels: earningData.labels,
+          datasets: earningData.datasets.map(dataset => ({
+            ...dataset,
+            pointRadius: 0,
+            tension: 0.4,
+            fill: false
+          }))
+        });
       } catch (err: any) {
         console.error('Error al obtener los datos de la API:', err.message);
         setError(err.message);
       } finally {
         setIsLoading(false);
       }
-    }
+    };
 
     fetchDataAndDrawChart();
   }, []);
@@ -97,35 +67,21 @@ const EarningData: React.FC = () => {
   }
 
   return data ? (
-    <div style={{ width: '100%', maxWidth: '600px',height:'200px' }}> {/* Ancho máximo y responsive */}
+    <div style={{ width: '100%', maxWidth: '600px', height: '200px' }}>
       <Line
         data={data}
         options={{
           maintainAspectRatio: false,
           responsive: true,
           plugins: {
-            legend: {
-              display: false
-            }
+            legend: { display: false }
           },
           scales: {
             y: {
-              ticks: {
-                color: '#AEAFBB'
-              },
-              grid: {
-                display: true,
-                color: 'rgba(174, 175, 187, 0.1)'
-              }
+              ticks: { color: '#AEAFBB' },
+              grid: { display: true, color: 'rgba(174, 175, 187, 0.1)' }
             },
-            x: {
-              ticks: {
-                color: '#AEAFBB'
-              },
-              grid: {
-                display: false
-              }
-            }
+            x: { ticks: { color: '#AEAFBB' }, grid: { display: false } }
           }
         }}
       />
