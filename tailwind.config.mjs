783cf40822db78c42b53f95553cb4c74936c478c
Luishiño <pericena12@gmail.com>
2024-07-01 08:15:20 -0400

    FOOD-71: Darkmode is being implemented for the admin view, the tailwind configuration files were created, as it is also already configured for astro, no modifications are required, the documentation is being followed. from confluence https://locotoinnovations.atlassian.net/wiki/x/N4C4AQ The BaseAdminLayout file was modified where the logic of saving in storage is being implemented as well as the action of changing colors.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,21 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-	],
+/**
+ * Tailwind CSS
+ * @type {import('tailwindcss').Config}
+ */
+module.exports = {
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        adminwhiteLight: '#ff5c28',
+        adminBlueLight: '#b1b1b1',
+        adminBlueDark: '#090D28'
+      },
+      fontFamily: {},
+      fontSize: {}
+    }
+  },
+
+  plugins: []
 }
